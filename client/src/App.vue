b98a3e2119071af9b4f4650eefa79d6484d886cb
rudyfebrianthomas <rudyfebrian6@gmail.com>
2019-06-28 09:59:22 +0700

    vuex

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,39 @@
 <template>
   <div id="app">
     <div class="container">
-      <div class="row">
-        <div class="col d-flex justify-content-center form-input">
-          <userInput/>
+      <div v-if="(roomstatus == 'pending' || roomstatus == '') && !createdRoom">
+        <div class="row">
+          <div class="col d-flex justify-content-center form-input">
+            <userInput/>
+          </div>
+        </div>
+        <div class="row border">
+          <div class="col d-flex justify-content-center">
+            <roomlist/>
+          </div>
         </div>
       </div>
-      <div class="row border">
-        <div class="col d-flex justify-content-center">
-          <roomlist/>
-        </div>
+      <div v-if="createdRoom">
+        <h1>WAITING FOR PLAYER</h1>
       </div>
-      <div class="row border">
-        <div class="col d-flex justify-content-center">
-          <pokemonlist/>
+      <div v-if="roomstatus == 'start'">
+        <h1>GAME START</h1>
+        <div class="row border">
+          <div class="col d-flex justify-content-center">
+            <pokemonlist/>
+          </div>
         </div>
-      </div>
-      <div class="row border">
-        <div class="col d-flex justify-content-center">
-          <playing/>
+        <div class="row border">
+          <div class="col d-flex justify-content-center">
+            <playing/>
+          </div>
         </div>
-      </div>
-      <div class="row border">
-        <div class="col d-flex justify-content-center">
-          <winner/>
+        <div class="row border">
+          <div class="col d-flex justify-content-center">
+            <winner/>
+          </div>
         </div>
+        <input type="button" value="Done" @click="gameFinish">
       </div>
     </div>
   </div>
@@ -36,6 +45,7 @@ import roomlist from "@/components/roomlist.vue";
 import pokemonlist from "@/components/pokemonlist.vue";
 import playing from "@/components/playing.vue";
 import winner from "@/components/winner.vue";
+import db from "./config/firebase";
 
 export default {
   name: "app",
@@ -48,8 +58,75 @@ export default {
   },
   data() {
     return {
-      loginPage: ""
+      namaroom: "",
+      nama: "",
+      roomstatus: "",
+      createdRoom: false
     };
+  },
+  computed: {
+    listrooms() {
+      return this.$store.state.rooms;
+    },
+    currentroom() {
+      return this.$store.state.currentRoom;
+    }
+  },
+  methods: {
+    login() {
+      localStorage.setItem("name", this.nama);
+      this.name = "";
+    },
+    gameFinish() {
+      this.$store.dispatch("GAMEFINISH", this.currentroom.id);
+    },
+    createRoom() {
+      let data = {
+        name: this.namaroom,
+        players: [
+          {
+            name: localStorage.name,
+            status: "player 1"
+          }
+        ],
+        status: "pending",
+        pokemons: []
+      };
+      this.$store.dispatch("CREATEROOM", data);
+      this.namaroom = "";
+      this.createdRoom = true;
+    },
+    joinRoom(id) {
+      console.log("dapat current room");
+      this.$store.dispatch("GETCURRENTROOM", id);
+    },
+    joincurrentroom(id) {
+      console.log("masuk", this.currentroom.players);
+      if (
+        localStorage.name == this.currentroom.players.name ||
+        this.currentroom.players.length == 2
+      ) {
+        alert("cannot join");
+      } else {
+        alert("join");
+        this.$store.dispatch("UPDATEROOM", id);
+      }
+    }
+  },
+  watch: {
+    currentroom() {
+      console.log("current room berubah", this.currentroom.status);
+      this.roomstatus = this.currentroom.status;
+      if (this.currentroom.status == "start") {
+        this.createdRoom = false;
+      }
+      if (this.currentroom !== "") {
+        this.$store.dispatch("GETCURRENTROOM", this.currentroom.id);
+      }
+    }
+  },
+  mounted() {
+    this.$store.dispatch("GETALLROOMS");
   }
 };
 </script>
--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,88 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import db from './config/firebase.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    rooms: [],
+    currentRoom: ''
   },
   mutations: {
-
+    ROOMLIST(state, data) {
+      state.rooms = data
+    },
+    CURRENTROOM(state, data) {
+      state.currentRoom = data
+    }
   },
   actions: {
-
+    CREATEROOM(context, data) {
+      db.collection('Room')
+        .add(data)
+        .then((doc) => {
+          context.dispatch('GETCURRENTROOM', doc.id)
+        })
+        .catch((err) => {
+          console.log(err)
+        });
+    },
+    GETCURRENTROOM(context, id) {
+      db.collection('Room')
+        .doc(id)
+        .get()
+        .then((doc) => {
+          context.commit('CURRENTROOM', {id: doc.id, ...doc.data()})
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    GETALLROOMS(context) {
+      db.collection('Room')
+        .onSnapshot(querySnapshot => {
+          var room = []
+          querySnapshot.forEach(function (doc) {
+            room.push({
+              id: doc.id,
+              ...doc.data()
+            })
+          })
+          context.commit('ROOMLIST', room)
+        })
+    },
+    UPDATEROOM(context, id) {
+      context.state.currentRoom.players.push({
+        name: localStorage.name,
+        status: 'player 2'
+      })
+      db.collection('Room')
+        .doc(id)
+        .update({
+          status: 'start',
+          players: context.state.currentRoom.players
+        })
+        .then(() => {
+          context.dispatch('GETCURRENTROOM', id)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    GAMEFINISH(context, id){
+      db.collection('Room')
+      .doc(id)
+      .update({
+        status: 'start',
+        players: context.state.currentRoom.players
+      })
+      .then(() => {
+        context.dispatch('GETCURRENTROOM', id)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
   }
 })
