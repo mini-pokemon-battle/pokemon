676eff55793a2486da4742fd3d425e514946658e
wildannugroho <wildanagung34@gmail.com>
2019-06-29 09:22:24 +0700

    ilangin config. maap ni di email mulu ama google

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+require('dotenv').config
+
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBMgzblqqn56mhhq_HMZtT-tNlRZ1eZCiQ",
-    authDomain: "group-project-pokemon.firebaseapp.com",
-    databaseURL: "https://group-project-pokemon.firebaseio.com",
-    projectId: "group-project-pokemon",
-    storageBucket: "group-project-pokemon.appspot.com",
-    messagingSenderId: "229416045485",
-    appId: "1:229416045485:web:7f268df45e8a2e90"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECT,
+    storageBucket: process.envBUCKET,
+    messagingSenderId: process.env.OKOK,
+    appId: process.env.appID
   };
 
 firebase.initializeApp(firebaseConfig)
